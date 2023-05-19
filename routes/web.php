40dc51b13c659436ec3289757221503b655c0812
Elyas Afghan <afghan.elyas1@gmail.com>
2023-05-19 23:08:17 +0430

    laravel fileSystem classes

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+
+use App\Models\Post;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts' , [
+        'posts' => Post::all()
+    ]);
+});
+
+
+Route::get('posts/{post}', function ($slug) {
+    $post = Post::find($slug);
+    return view('post', ['post' => $post]);
+
+})->where('post', '[A-z_\-]+');
+
+
