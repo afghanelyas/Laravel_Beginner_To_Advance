8c1f7b696bc9da0071ccbe4a00645903a408855c
Elyas Afghan <afghan.elyas1@gmail.com>
2023-05-21 08:39:15 +0430

    collection sorting and caching refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
+
 class Post
 {
     public $title;
@@ -24,15 +25,19 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts/")))
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn($document) => new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        ));
+        // return the cache file 
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts/")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
     }
 
     public static function find($slug)
