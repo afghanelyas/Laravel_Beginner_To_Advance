de7b445c75c46a1ff1260dc9a2f060ca3e492827
Elyas Afghan <afghan.elyas1@gmail.com>
2023-05-21 09:16:00 +0430

    making layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="app.css"/>
-    <title>My blog</title>
-</head>
-<body>
+@extends ('layout')
+
+@section('content')
 
 @foreach ($posts as $post)
 <article>
@@ -21,5 +14,4 @@
 </article>
 @endforeach
 
-</body>
-</html>
+@endsection
