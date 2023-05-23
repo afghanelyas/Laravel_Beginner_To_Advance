9a522f889dc899f37d93ce79ef35fae31fa6116b
afghanelyas <afghan.elyas1@gmail.com>
2023-05-23 19:05:09 +0430

    Turbo boosts with model facories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,64 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-        ]);
-        $family = Category::create([
-            'name' => 'Family',
-           'slug' => 'family',
-        ]);
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
+        $user = User::factory()->create([
+            'name' => 'Joha'
         ]);
 
-        Post::create([
+        Post::factory(5)->create([
             'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My First Post',
-            'slug' => 'my-first-post',
-            'excerpt' =>  '<p> Lorem ipsum dolor sit amet consectetur adipisic</p>' ,
-            'body' => '<p> Lorem ipsum dolor sit amet consectetur adipisic</p>',
-
         ]);
-        
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Second Post',
-            'slug' => 'my-second-post',
-            'excerpt' => '<p> Lorem ipsum dolor sit amet consectetur adipisic</p>',
-            'body' => '<p> Lorem ipsum dolor sit amet consectetur adipisic</p>',
-
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Third Post',
-            'slug' => 'my-third-post',
-            'excerpt' => '<p> Lorem ipsum dolor sit amet consectetur adipisic</p>',
-            'body' => '<p> Lorem ipsum dolor sit amet consectetur adipisic</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Fourth Post',
-            'slug' => 'my-fourth-post',
-            'excerpt' => '<p> Lorem ipsum dolor sit amet consectetur adipisic</p>',
-            'body' => '<p> Lorem ipsum dolor sit amet consectetur adipisic</p>'
-        ]);
-
-
         
     }
 }
