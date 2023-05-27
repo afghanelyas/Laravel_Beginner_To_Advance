260ac0de3e0af4b9e863c3050b1f6a5f18f4bdc7
afghanelyas <afghan.elyas1@gmail.com>
2023-05-28 00:18:20 +0430

    converto to hmtl and css blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,21 @@
 @extends('components.layout')
 
 @section('content')
-    <article>
+    <article class="bg-white border border-gray-200 mb-5 p-5 text-lg shadow-md leading-loose ">
 
-        <h1>
+        <h1 class="text-2xl underline">
             {{ $post->title }}
         </h1>
 
         <p>
-            By <a href="/authors/{{ $post->author->name }}"> {{ $post->author->name }} in </a> <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            By <a href="/authors/{{ $post->author->name }}" class="underline"> {{ $post->author->name }} in </a> <a href="/categories/{{ $post->category->slug }}" class="underline">{{ $post->category->name }}</a>
         </p>
 
         <div>
             {!! $post->body !!}
         </div>
 
-        <a href="/">Go Back</a>
+        <a href="/" class="underline" >Go Back</a>
         
     </article>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,19 @@
 @extends('components.layout')
 
 @section('content')
+    
     @foreach ($posts as $post)
-        <article>
-            <h2>
+
+        <!-- // create a categroy form -->
+        
+
+        <article class="bg-white border border-gray-200 mb-5 p-5 text-lg shadow-md leading-loose ">
+            <h2 class="text-2xl underline">
                 <a href="/posts/{{ $post->slug }}">{!! $post->title !!}</a>
             </h2>
 
             <p>
-            By <a href="/authors/{{ $post->author->username }}"> {{ $post->author->name }} in </a> <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            By <a class="underline" href="/authors/{{ $post->author->username }}"> {{ $post->author->name }} in </a> <a class="underline" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
         </p>
 
             <div>
