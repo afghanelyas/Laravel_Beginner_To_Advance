7d8903b4237a4b54c951f31ff9d8f5a5fad60e31
afghanelyas <afghan.elyas1@gmail.com>
2023-05-30 14:08:51 +0430

    automatic password hashing with matators

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,8 +37,11 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = bcrypt($password);
+    }
 
     public function posts()
     {
