234f0e0a7d9af883944fb4d677a66af9ff158d73
afghanelyas <afghan.elyas1@gmail.com>
2023-05-31 09:10:10 +0430

    table consistency and forign key constriants

--- database/migrations/2023_05_31_040657_create_post_comments_table.php
+++ database/migrations/2023_05_31_040657_create_post_comments_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('post_comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id');
-            $table->string('title');
-            $table->string('slug')->unique();
-            $table->text('excerpt');
             $table->text('body');
             $table->timestamps();
-            $table->timestamp('published_at')->nullable();
         });
     }
 
@@ -29,7 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-    
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('post_comments');
     }
 };
