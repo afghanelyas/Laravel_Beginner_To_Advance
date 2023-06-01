221343e8dc493784c6b4cb7a737d7c23567d7127
afghanelyas <afghan.elyas1@gmail.com>
2023-06-01 21:57:28 +0430

    A litle clean up the code

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,32 +21,7 @@
 
 <!-- create design form for comment -->
 
-@auth
-<form action="/posts/{{ $post->slug }}/comments" method="POST" class="border mb-6 border-gray-200 p-6  rounded-xl">
-    @csrf
-
-    <header class="flex items-center">
-        <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
-        <h2 class="ml-4">Want to participate?</h2>
-    </header>
-
-    <div class="mt-6">
-        <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5"
-            placeholder="Quick, thing of something to say!" required></textarea>
-    </div>
-
-    <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-        <button type="submit"
-            class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-    </div>
-
-</form>
-@else
-<p class="font-semibold">
-    <a href="/login" class="underline">Login</a> to leave a comment
-</p>
-@endauth
-
+@include('posts._add-comment-form')
 
 <!-- foreach the comments -->
 @foreach ($post->comment as $comment)
